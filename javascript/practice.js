4c7ce55537bf158056673b46f0c0c0f6ff440107
balamurugandev05 <rxakash05@gmail.com>
2023-12-20 21:12:39 +0530

    js

---
+++ javascript/practice.js
@@ -0,0 +1,21 @@
+
+// function g(){
+//     s="Gri n";
+//     console.log(s);
+// }
+// g();
+
+let a=1;
+var b=2;
+const c=3;
+
+function n(){
+    let a=1;
+    var b=2;
+    const c=3;
+    console.log(`function ${a} ${b} ${c}`);
+}
+n();
+
+
+console.log(`outer page ${a} ${b} ${c}`);
