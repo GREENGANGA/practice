c385eb279a189f763f56c7fb38f6f5ee991ea05e
balamurugandev05 <rxakash05@gmail.com>
2023-12-20 21:36:10 +0530

    some

---
+++ sample/script.js
@@ -0,0 +1,6 @@
+// const headr=Document.getElementsByClass(".banner"); //wrong
+
+console.log(document.querySelector(".header-wrapper>div li a"))
+const li=document.querySelector("li");
+
+document.querySelector("li:nth-child(1)").style.color="green";
